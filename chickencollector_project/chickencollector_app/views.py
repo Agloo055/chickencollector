30b1316b601538bbb5f0549e223b80fd08b1f25f
Joberman055 <jacksonoberman@gmail.com>
2023-10-24 21:32:43 -0700

    add view chickens list

--- chickencollector_project/chickencollector_app/views.py
+++ chickencollector_project/chickencollector_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+chickens = [
+    {'name': "Sage", 'breed': 'Olive Egger', 'description': 'Grey with Beard', 'age': 3},
+    {'name': "Sclepi", 'breed': 'Golden Lace', 'description': 'Black and Golden', 'age': 3},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'chickens/home.html')
 
 def about(request):
     return render(request, 'chickens/about.html')
+
+def chickens_index(request):
+    return render(request, 'chickens/index.html', {
+        'chickens': chickens
+    })
