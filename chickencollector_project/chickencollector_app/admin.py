8f69613583c26f183ff73b711f48a48c7ed17fa9
Joberman055 <jacksonoberman@gmail.com>
2023-10-28 16:53:07 -0700

    add laying model

--- chickencollector_project/chickencollector_app/admin.py
+++ chickencollector_project/chickencollector_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Chicken
+from .models import Chicken, Laying
 
 # Register your models here.
 admin.site.register(Chicken)
+admin.site.register(Laying)
--- chickencollector_project/chickencollector_app/models.py
+++ chickencollector_project/chickencollector_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+TIMES = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('L', 'Late Afternoon')
+)
+
 # Create your models here.
 class Chicken (models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +19,16 @@ class Chicken (models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk':self.id})
+    
+class Laying (models.Model):
+    date = models.DateField('laying Date')
+    time = models.CharField(
+        max_length=1,
+        choices=TIMES,
+        default=TIMES[0][0]
+    )
+
+    chicken = models.ForeignKey(Chicken, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
